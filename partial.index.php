f0ab5d53909f30734352b8ebdd2e6574c9a22b77
thomas493 <47501130+thomas493@users.noreply.github.com>
2020-02-19 22:16:07 +0100

    Update partial.index.php

--- partial.index.php
+++ partial.index.php
@@ -1,378 +1,378 @@
 <div id="intro2" class="etage">
     <div class="textBlock">
-<h1>La Coalition climat</h1>
-<p></p>
+        <h1>La Coalition climat</h1>
+        <p></p>
 
-<h1>le pacte</h1>
-<a href="lien">Consulter le document</a>
+        <h1>le pacte</h1>
+        <a href="lien">Consulter le document</a>
 
-<h1>Réponse des candidats</h1>
+        <h1>R&eacute;ponse des candidats</h1>
 
-<h2> Amfreville-la-Mi-Voie</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Anneville-Ambourville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Bardouville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Belbeuf</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Berville-sur-Seine</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Bihorel</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Bois-Guillaume</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Bonsecours</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Boos</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Canteleu</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Caudebec-lès-Elbeuf</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Cléon</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Darnétal</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Déville-lès-Rouen</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Duclair</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Elbeuf</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Épinay-sur-Duclair</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Fontaine-sous-Préaux</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Franqueville-Saint-Pierre</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Freneuse</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Gouy</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Grand-Couronne</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Hautot-sur-Seine</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Hénouville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Houppeville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Isneauville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> Jumièges</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2> La Bouille</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>La Londe</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>La Neuville-Chant-d'Oisel</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Le Grand-Quevilly</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Le Houlme</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Le Mesnil-Esnard</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Le Mesnil-sous-Jumièges</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Le Petit-Quevilly</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Le Trait</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Les Authieux-sur-le-Port-Saint-Ouen</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Malaunay</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Maromme</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Mont-Saint-Aignan</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Montmain</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Moulineaux</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Notre-Dame-de-Bondeville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Oissel</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Orival</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Petit-Couronne</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Quevillon</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Quévreville-la-Poterie</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Roncherolles-sur-le-Vivier</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Rouen</h2>
-<ul>
-    <li>Au coeur de Rouen</li>
-    <li>Ensemble pour Rouen </li><a href="lien"></a>
-    <li>Fiers de Rouen</li>
-    <li>Nos vies, pas leurs profits. Rouen en lutte</li>
-    <li>Réenchantons Rouen</li>
-    <li>Rouen Autrement</li>
-    <li>Rouen, Notre commune</li>
-    <li>PA Rouen</li>
-    <li>RN Rouen</li>
-</ul>
-<h2>Sahurs</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Aubin-Celloville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Aubin-Épinay</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Aubin-lès-Elbeuf</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Étienne-du-Rouvray</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Jacques-sur-Darnétal</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Léger-du-Bourg-Denis</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Martin-de-Boscherville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Martin-du-Vivier</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Paër</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Pierre-de-Manneville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Pierre-de-Varengeville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Saint-Pierre-lès-Elbeuf</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Sainte-Marguerite-sur-Duclair</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Sotteville-lès-Rouen</h2>
-<ul>
-    <li>Ensemble pour Sotteville</li><a href="lien"></a>
-    <li>Inventons Sotteville</li>
-    <li>Rassemblée, Sotteville avance</li>
-    <li>Rassemblement pour Sotteville</li>
-    <li>Le Renouveau Sottevillais</li>
-    
-</ul>
-<h2>Sotteville-sous-le-Val</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Tourville-la-Rivière</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Val-de-la-Haye</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Yainville</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Ymare</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-<h2>Yville-sur-Seine</h2>
-<ul>
-    <li>Liste 1</li><a href="lien"></a>
-    <li>liste 2</li>
-</ul>
-</div>
+        <h2> Amfreville-la-Mi-Voie</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Anneville-Ambourville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Bardouville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Belbeuf</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Berville-sur-Seine</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Bihorel</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Bois-Guillaume</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Bonsecours</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Boos</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Canteleu</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Caudebec-l&egrave;s-Elbeuf</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Cl&eacute;on</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Darn&eacute;tal</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> D&eacute;ville-l&egrave;s-Rouen</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Duclair</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Elbeuf</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> &Eacute;pinay-sur-Duclair</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Fontaine-sous-Pr&eacute;aux</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Franqueville-Saint-Pierre</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Freneuse</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Gouy</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Grand-Couronne</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Hautot-sur-Seine</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> H&eacute;nouville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Houppeville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Isneauville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> Jumi&egrave;ges</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2> La Bouille</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>La Londe</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>La Neuville-Chant-d'Oisel</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Le Grand-Quevilly</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Le Houlme</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Le Mesnil-Esnard</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Le Mesnil-sous-Jumi&egrave;ges</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Le Petit-Quevilly</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Le Trait</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Les Authieux-sur-le-Port-Saint-Ouen</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Malaunay</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Maromme</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Mont-Saint-Aignan</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Montmain</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Moulineaux</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Notre-Dame-de-Bondeville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Oissel</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Orival</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Petit-Couronne</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Quevillon</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Qu&eacute;vreville-la-Poterie</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Roncherolles-sur-le-Vivier</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Rouen</h2>
+        <ul>
+            <li>Au coeur de Rouen</li>
+            <li>Ensemble pour Rouen </li><a href="lien"></a>
+            <li>Fiers de Rouen</li>
+            <li>Nos vies, pas leurs profits. Rouen en lutte</li>
+            <li>R&eacute;enchantons Rouen</li>
+            <li>Rouen Autrement</li>
+            <li>Rouen, Notre commune</li>
+            <li>PA Rouen</li>
+            <li>RN Rouen</li>
+        </ul>
+        <h2>Sahurs</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Aubin-Celloville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Aubin-&Eacute;pinay</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Aubin-l&egrave;s-Elbeuf</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-&Eacute;tienne-du-Rouvray</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Jacques-sur-Darn&eacute;tal</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-L&eacute;ger-du-Bourg-Denis</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Martin-de-Boscherville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Martin-du-Vivier</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Pa&euml;r</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Pierre-de-Manneville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Pierre-de-Varengeville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Saint-Pierre-l&egrave;s-Elbeuf</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Sainte-Marguerite-sur-Duclair</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Sotteville-l&egrave;s-Rouen</h2>
+        <ul>
+            <li>Ensemble pour Sotteville</li><a href="lien"></a>
+            <li>Inventons Sotteville</li>
+            <li>Rassembl&eacute;e, Sotteville avance</li>
+            <li>Rassemblement pour Sotteville</li>
+            <li>Le Renouveau Sottevillais</li>
+
+        </ul>
+        <h2>Sotteville-sous-le-Val</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Tourville-la-Rivi&egrave;re</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Val-de-la-Haye</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Yainville</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Ymare</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+        <h2>Yville-sur-Seine</h2>
+        <ul>
+            <li>Liste 1</li><a href="lien"></a>
+            <li>liste 2</li>
+        </ul>
+    </div>
 </div>
