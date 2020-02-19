80656b291049a78a9d1a4952992f647e7f27bdbf
thomas493 <47501130+thomas493@users.noreply.github.com>
2020-02-19 21:59:58 +0100

    Update partial.index.php

--- partial.index.php
+++ partial.index.php
@@ -1,17 +1,5 @@
 <div id="intro2" class="etage">
     <div class="textBlock">
-        <h2>Nouvelle marche organis&eacute;e &agrave; Rouen le samedi 30 novembre!!!</h2>
-        <p>Une nouvelle marche est en préparation pour le samedi 30 novembre.</p>
-        <p>Départ à 10h30 devant l'église St Sever</p>
-        
-        <p>L'événement facebook est ici: <a href="https://www.facebook.com/events/2140419159586637/">https://www.facebook.com/events/2140419159586637/</a> </p>
-        <h2>Cette marche est organisée par la coalition climat</h2>
-        <p>La coalition  climat est un regroupement
-            d'associations, de collectifs citoyens et
-            d'organisations syndicales</p>
-        <p>Retrouvez les associations qui la constituent dans le document <a href="doc/FlyerCoalition.pdf">accessible ici.</a> Vous trouverez aussi les coordonnées de chacune d'elles.</p>
-    </div>
-</div>
 <h1>La Coalition climat</h1>
 <p></p>
 
@@ -386,4 +374,5 @@
     <li>Liste 1</li><a href="lien"></a>
     <li>liste 2</li>
 </ul>
-
+</div>
+</div>
